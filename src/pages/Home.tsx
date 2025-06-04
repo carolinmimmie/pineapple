0a4a0fe1720c501d936f57a17ac92798b4f555bd
carolinmimmie <carolinmimmie@gmail.com>
2025-06-04 22:04:03 +0200

    renamed src to images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,17 @@
 const Home = () => {
   const images = [
-    "../../public/assets/home1.jpg",
-    "../../public/assets/home2.jpg",
-    "../../public/assets/home3.jpg",
-    "../../public/assets/home4.jpg",
-    "../../public/assets/home5.jpg",
-    "../../public/assets/home6.jpg",
-    "../../public/assets/home7.jpg",
-    "../../public/assets/home8.jpg",
-    "../../public/assets/home9.jpg",
-    "../../public/assets/home10.jpg",
-    "../../public/assets/home11.jpg",
-    "../../public/assets/home12.jpg",
+    "/assets/home1.jpg",
+    "/assets/home2.jpg",
+    "/assets/home3.jpg",
+    "/assets/home4.jpg",
+    "/assets/home5.jpg",
+    "/assets/home6.jpg",
+    "/assets/home7.jpg",
+    "/assets/home8.jpg",
+    "/assets/home9.jpg",
+    "/assets/home10.jpg",
+    "/assets/home11.jpg",
+    "/assets/home12.jpg",
   ];
   return (
     <div className="home">
