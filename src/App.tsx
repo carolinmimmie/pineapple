dcfe314c1adfd98f314aac45f59e144ba871f5dc
carolinmimmie <carolinmimmie@gmail.com>
2025-06-09 09:45:06 +0200

    added google maps

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
+import "leaflet/dist/leaflet.css";
 
 function App() {
   return (
--- src/pages/FindUs.tsx
+++ src/pages/FindUs.tsx
@@ -1,7 +1,33 @@
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "100%",
+  height: "400px",
+};
+
+const center = {
+  lat: 59.3057,
+  lng: 18.1095,
+};
+
 const FindUs = () => {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyCi36RM6ZvbcSWmFZG98aR8j8wAGpQ2Wzs",
+  });
+
+  if (!isLoaded) {
+    return <div>Laddar karta...</div>;
+  }
+
   return (
-    <div>
-      <h2>Hitta hit</h2>
+    <div className="find-us">
+      <section>
+        <h2>Hitta hit</h2>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+          <Marker position={center} />
+        </GoogleMap>
+        <h4>Hammarbysjöstad 32, 120 30 Stockholm</h4>
+      </section>
     </div>
   );
 };
